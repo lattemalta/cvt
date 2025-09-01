f455a9215ea2b24d14d80e4341db912dabb9c651
lattemalta <latte.malta@gmail.com>
2025-09-02 02:04:42 +0900

    ログ機構を追加
    Rich使用のカラフルなログ出力
    画像処理用依存関係追加(opencv-python, pillow)

---
+++ src/cvt/logger.py
@@ -0,0 +1,10 @@
+from rich.logging import RichHandler
+from logging import getLogger
+import logging
+
+
+def setup_logger() -> None:
+    root_logger = getLogger()
+    root_logger.setLevel("INFO")
+    root_logger.addHandler(RichHandler(omit_repeated_times=False, rich_tracebacks=True))
+    logging.captureWarnings(True)
